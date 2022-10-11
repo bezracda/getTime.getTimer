9746abcdfa3067d661692438a57e9239eed78bc6
bezracda <bezracda@gmail.com>
2022-10-12 01:28:08 +0300

    Version 1.1

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,10 +1,12 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+@Component
+public class Dog extends Animal{
 
-public abstract class Animal {
-
+    public String toString() {
+        return "Im a Dog";
+    }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,18 @@
 package app.model;
 
+import org.springframework.stereotype.Component;
+
+import java.sql.Time;
+
+@Component
 public class Timer {
 
+
     private Long nanoTime = System.nanoTime();
+    private Time time;
 
     public Long getTime() {
         return nanoTime;
     }
+
 }
